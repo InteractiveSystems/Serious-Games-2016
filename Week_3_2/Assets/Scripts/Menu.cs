9f10055a3eea6e6f43b8f5329bbe7f9d1cd31ce7
dyadica <sjb@dyadica.co.uk>
2016-01-26 18:36:13 +0000

    Week 3
    Upload of Week 3 content
    - canvas
    - buttons
    - jump fixes

---
+++ Week_3_2/Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Those using Unity 5.3 and above. Please use the 
+// SceneManager loading method provided by the
+// following using statement.
+
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void SelectPNC()
+    {
+        PlayerPrefs.SetString("Mode", "PNC");
+
+        // For unity versions < 5.3
+        // Application.LoadLevel("Gameplay");
+
+        // For unity versions > 5.3
+        SceneManager.LoadScene("Gameplay");
+        
+    }
+
+    public void SelectKNM()
+    {
+        PlayerPrefs.SetString("Mode", "KNM");
+
+        // For unity versions < 5.3
+        // Application.LoadLevel("Gameplay");
+
+        // For unity versions > 5.3
+        SceneManager.LoadScene("Gameplay");
+    }
+}
